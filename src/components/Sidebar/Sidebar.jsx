d13157a3c06cf99c239225f269c9f4b72a10c083
German Cutraro <germancutraro@hotmail.com>
2020-02-11 10:35:47 -0300

    remove side icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { SideContainer } from './sidebarStyles';
+import { SideContainer, Items, Item, ItemText } from './sidebarStyles';
 
 const Sidebar = () => {
-  return <SideContainer></SideContainer>;
+  return (
+    <SideContainer>
+      <Items>
+        <Item>
+          {/* <MaterialIcon icon='home' color={colorPalette.grey._50} /> */}
+          <ItemText>Home</ItemText>
+        </Item>
+        <Item>
+          {/* <MaterialIcon icon='headset' color={colorPalette.grey._50} /> */}
+          <ItemText>Browse</ItemText>
+        </Item>
+        <Item>
+          {/* <MaterialIcon icon='radio' color={colorPalette.grey._50} /> */}
+          <ItemText>Radio</ItemText>
+        </Item>
+      </Items>
+    </SideContainer>
+  );
 };
 
 export default Sidebar;
--- src/components/Sidebar/sidebarStyles.js
+++ src/components/Sidebar/sidebarStyles.js
@@ -7,3 +7,28 @@ export const SideContainer = styled.div`
   display: flex;
   justify-content: flex-start;
 `;
+
+export const Items = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0.5rem;
+  height: 2em;
+  width: 100%;
+  cursor: pointer;
+`;
+
+export const ItemText = styled.p`
+  color: #b3b3b3;
+  font-size: 0.9rem;
+  font-weight: 700;
+  padding: 0.6em;
+  &:hover {
+    color: #fff;
+  }
+`;
