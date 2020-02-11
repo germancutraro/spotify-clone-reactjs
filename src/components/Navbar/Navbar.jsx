f5b3ec3c1944d99a3754f56508708ef2727bf154
German Cutraro <germancutraro@hotmail.com>
2020-02-10 21:20:21 -0300

    navbar initial styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { NavContainer } from './navbarStyles';
+import {
+  NavContainer,
+  NavRightWrapper,
+  NavLeftWrapper,
+  UpgradeButton,
+  Avatar,
+  UpgradeText
+} from './navbarStyles';
 
 const Navbar = () => {
-  return <NavContainer></NavContainer>;
+  return (
+    <NavContainer>
+      <NavRightWrapper>
+        <input type='text' placeholder='Name...' />
+      </NavRightWrapper>
+      <NavLeftWrapper>
+        <UpgradeButton>
+          <UpgradeText>upgrade</UpgradeText>
+        </UpgradeButton>
+        <Avatar src='https://profile-images.scdn.co/images/userprofile/default/e3a22d031a8b9245d40fe20860461a21d68a198b' />
+      </NavLeftWrapper>
+    </NavContainer>
+  );
 };
 
 export default Navbar;
--- src/components/Navbar/navbarStyles.js
+++ src/components/Navbar/navbarStyles.js
@@ -1,7 +1,45 @@
 import styled from 'styled-components';
 
 export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100vw;
+  height: 80px;
   background-color: #000;
-  height: 50px;
+`;
+
+export const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  margin: 1rem;
+`;
+
+export const UpgradeButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 110px;
+  height: 24px;
+  border-radius: 10px;
+  border: 1px solid #fff;
+`;
+
+export const UpgradeText = styled.p`
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 10.8px;
+  font-weight: 700;
+  letter-spacing: 2px;
+`;
+
+export const NavLeftWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const NavRightWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
