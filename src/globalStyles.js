db2e9d6dccde5f16ca017e04aee68b2a587dd902
German Cutraro <germancutraro@hotmail.com>
2020-02-11 10:11:11 -0300

    fonts

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
+import Montserrat from './assets/fonts/Montserrat-Regular.ttf';
+
 const GlobalStyle = createGlobalStyle`
   ${normalize}
   html,
   body {
     height: 100%;
     background-color: #0C0C0C;
-    /* font-family: 'Roboto', sans-serif; */
+    font-family: Montserrat, sans-serif;
   }
 
   * ,
@@ -18,6 +20,11 @@ const GlobalStyle = createGlobalStyle`
 
   #root {
     height: 100%;
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    src: url(${Montserrat});
   }
  
 `;
