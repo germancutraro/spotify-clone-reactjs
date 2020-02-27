d398a26a6c7a1370bae43dc1b78a54d6c19c629b
German Cutraro <germancutraro@hotmail.com>
2020-02-27 09:45:50 -0300

    sidebar link fixed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -44,13 +44,13 @@ const Sidebar = () => {
           </ItemLink>
         </Item>
         <Item>
-          <ItemLink to='app/search'>
+          <ItemLink to='/app/search'>
             <Search fill='#a8a8a8' width={23} height={23} />
             <ItemText>Search</ItemText>
           </ItemLink>
         </Item>
         <Item>
-          <ItemLink to='app/collection/playlists'>
+          <ItemLink to='/app/collection/playlists'>
             <LibraryIcon fill='#a8a8a8' width={23} height={23} />
             <ItemText>Your Library</ItemText>
           </ItemLink>
