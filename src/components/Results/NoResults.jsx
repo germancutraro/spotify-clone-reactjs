b706906ed07c68edcbb5e97dc426e55b11f660d0
German Cutraro <germancutraro@hotmail.com>
2020-02-29 19:19:59 -0300

    song search fixed

---
+++ src/components/Results/NoResults.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NoResults = ({ searchValue }) => {
+  return (
+    <div>
+      <h2>No results found for "{searchValue}"</h2>
+      <small>
+        Please make sure your words are spelled correctly or use less or
+        different keywords.
+      </small>
+    </div>
+  );
+};
+
+export default NoResults;
