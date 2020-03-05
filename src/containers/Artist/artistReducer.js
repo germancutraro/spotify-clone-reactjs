c8895dd0ed489f171b02aa0c0837201b398eeab2
German Cutraro <germancutraro@hotmail.com>
2020-03-05 12:35:51 -0300

    artist saga complete

--- src/containers/Artist/artistReducer.js
+++ src/containers/Artist/artistReducer.js
@@ -2,6 +2,9 @@ import * as constants from './artistConstants';
 
 const initialState = {
   artist: {},
+  tracks: [],
+  albums: [],
+  related: [],
   loading: true,
   error: null
 };
@@ -9,11 +12,25 @@ const initialState = {
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case constants.GET_ARTIST_START:
-      return { ...state, loading: true, error: null, artist: {} };
+    case constants.GET_ARTIST_ALBUMS_START:
+    case constants.GET_ARTIST_TRACKS_START:
+    case constants.GET_ARTIST_RELATED_START:
+      return { ...state, loading: true, error: null };
+
     case constants.GET_ARTIST_SUCCESS:
       return { ...state, loading: false, artist: payload.artist };
+    case constants.GET_ARTIST_TRACKS_SUCCESS:
+      return { ...state, loading: false, tracks: payload.tracks };
+    case constants.GET_ARTIST_ALBUMS_SUCCESS:
+      return { ...state, loading: false, albums: payload.albums };
+    case constants.GET_ARTIST_RELATED_SUCCESS:
+      return { ...state, loading: false, related: payload.artists };
+
     case constants.GET_ARTIST_FAILURE:
-      return { ...state, loading: false, error: payload.error, artist: {} };
+    case constants.GET_ARTIST_TRACKS_FAILURE:
+    case constants.GET_ARTIST_ALBUMS_FAILURE:
+    case constants.GET_ARTIST_RELATED_FAILURE:
+      return { ...state, loading: false, error: payload.error };
     default:
       return state;
   }
--- src/containers/Artist/artistSaga.js
+++ src/containers/Artist/artistSaga.js
@@ -7,7 +7,6 @@ import * as services from './artistServices';
 function* getArtist({ payload: { id } }) {
   try {
     const artist = yield services.getArtist(id);
-    console.log(artist);
     if (artist) yield put(actions.getArtistSuccess({ artist }));
   } catch (err) {
     yield put(actions.getArtistFailure({ error: err.message }));
@@ -18,6 +17,54 @@ function* getArtistSaga() {
   yield takeLatest(constants.GET_ARTIST_START, getArtist);
 }
 
+// Get artist Tracks
+function* getArtistTopTracks({ payload: { id } }) {
+  try {
+    const tracks = yield services.getArtistTracks(id);
+    console.log(tracks);
+    if (tracks) yield put(actions.getArtistTracksSuccess({ tracks }));
+  } catch (err) {
+    yield put(actions.getArtistTracksFailure({ error: err.message }));
+  }
+}
+
+function* getArtistTopTracksSaga() {
+  yield takeLatest(constants.GET_ARTIST_TRACKS_START, getArtistTopTracks);
+}
+
+// Get Artist Albums
+function* getArtistAlbums({ payload: { id } }) {
+  try {
+    const albums = yield services.getArtistAlbums(id);
+    if (albums) yield put(actions.getArtistAlbumsSuccess({ albums }));
+  } catch (err) {
+    yield put(actions.getArtistAlbumsFailure({ error: err.message }));
+  }
+}
+
+function* getArtistAlbumsSaga() {
+  yield takeLatest(constants.GET_ARTIST_ALBUMS_START, getArtistAlbums);
+}
+
+// Get Artist Related
+function* getArtistRelated({ payload: { id } }) {
+  try {
+    const artists = yield services.getArtistsRelated(id);
+    if (artists) yield put(actions.getArtistRelatedSuccess({ artists }));
+  } catch (err) {
+    yield put(actions.getArtistRelatedFailure({ error: err.message }));
+  }
+}
+
+function* getArtistRelatedSaga() {
+  yield takeLatest(constants.GET_ARTIST_RELATED_START, getArtistRelated);
+}
+
 export default function* artistSaga() {
-  yield all([fork(getArtistSaga)]);
+  yield all([
+    fork(getArtistSaga),
+    fork(getArtistTopTracksSaga),
+    fork(getArtistAlbumsSaga),
+    fork(getArtistRelatedSaga)
+  ]);
 }
