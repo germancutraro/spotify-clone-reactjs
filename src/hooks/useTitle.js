5f322c42e2363c2c557f3a20c6dfed788b8cf348
German Cutraro <germancutraro@hotmail.com>
2020-03-13 18:12:46 -0300

    remove log

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 
-export default function useTitle(newTitle) {
+function useTitle(newTitle) {
   const [title, setTitle] = useState(document.title);
 
   useEffect(() => {
-    console.log(newTitle);
     if (newTitle && !newTitle.includes('undefined')) {
       setTitle(newTitle);
       document.title = title;
     }
   }, [title, newTitle]);
 }
+
+export default useTitle;
