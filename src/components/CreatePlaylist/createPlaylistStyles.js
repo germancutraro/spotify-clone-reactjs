1a6da50327873e042ab8f494a5be9907af84263a
BautiCutraro <bautistacutraro@gmail.com>
2020-03-14 23:20:59 -0300

    create playlist initial styles

--- src/components/CreatePlaylist/createPlaylistStyles.js
+++ src/components/CreatePlaylist/createPlaylistStyles.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const ModalContainer = styled.div``;
+export const ModalDialog = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 10000;
+  background-color: rgba(0, 0, 0, 0.9);
+`;
+
+export const ModalContainer = styled.div`
+  background-color: yellow;
+`;
