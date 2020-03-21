5d8f052684912696ea69acf8b4796ad6af582533
BautiCutraro <bautistacutraro@gmail.com>
2020-03-20 23:25:52 -0300

    create diferents folder for track

---
+++ src/components/Track/TrackLeft/TrackLeft.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TrackLeft = () => {
+  return <div></div>;
+};
+
+export default TrackLeft;
---
+++ src/components/Track/TrackrRight/TrackRight.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TrackRight = () => {
+  return <div></div>;
+};
+
+export default TrackRight;
