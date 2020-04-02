67303239856bba07528c8bb87c40e0b5bdc3c0a5
BautiCutraro <bautistacutraro@gmail.com>
2020-04-02 14:24:44 -0300

    more options in sidebar playlists

--- src/components/LibraryItem/PlaylistsItem.jsx
+++ src/components/LibraryItem/PlaylistsItem.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Item, PlaylistItemText } from '../Sidebar/sidebarStyles';
 import { useHistory } from 'react-router-dom';
+import MoreMenu from '../MoreMenu/MoreMenu';
 
 const PlaylistsItem = ({ id, name }) => {
+  const [isMoreMenuOpen, setIsMoreMenuOpen] = useState(false);
+  const [moreMenuPosition, setMoreMenuPosition] = useState([0, 0]);
+
   const history = useHistory();
+
+  const handleOnClickMore = e => {
+    e.preventDefault();
+
+    setIsMoreMenuOpen(true);
+    setMoreMenuPosition([e.pageX, e.pageY]);
+  };
   return (
-    <Item hasIcon={false}>
-      <PlaylistItemText
-        hasIcon={false}
-        onClick={() => history.push(`/app/playlist/${id}`)}
-        light
-      >
-        {name}
-      </PlaylistItemText>
-    </Item>
+    <>
+      <MoreMenu
+        open={isMoreMenuOpen}
+        close={() => setIsMoreMenuOpen(false)}
+        moreMenuPosition={moreMenuPosition}
+        items={[
+          { title: 'Iniciar Radio', onClick: () => alert('Iniciar radio') },
+          {
+            title: 'Guardar en canciones que te gustan',
+            onClick: () => alert('Guardar en canciones que te gustan')
+          },
+          {
+            title: 'Añadir a la cola',
+            onClick: () => alert('Añadir a la cola')
+          },
+          {
+            title: 'Añadir a playlist',
+            onClick: () => alert('Añadir a playlist')
+          },
+          {
+            title: 'Copiar enlace de la canción',
+            onClick: () => alert('Copiar enlace de la canción')
+          }
+        ]}
+      />
+      <Item hasIcon={false}>
+        <PlaylistItemText
+          hasIcon={false}
+          onClick={() => history.push(`/app/playlist/${id}`)}
+          light
+          onContextMenu={handleOnClickMore}
+        >
+          {name}
+        </PlaylistItemText>
+      </Item>
+    </>
   );
 };
 
